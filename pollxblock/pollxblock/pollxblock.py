9dd79a5a26cda1933ecc731aedb1077529eff2cb
yokose-ks <ayokose.gacco@gmail.com>
2014-08-08 18:54:03 +0900

    EDX-423: Second commit

--- pollxblock/pollxblock/pollxblock.py
+++ pollxblock/pollxblock/pollxblock.py
@@ -3,8 +3,9 @@
 import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, Integer
+from xblock.fields import Scope, Boolean, Dict, Integer, List, String
 from xblock.fragment import Fragment
+from django.template import Context, Template
 
 
 class PollXBlock(XBlock):
@@ -12,8 +13,19 @@ class PollXBlock(XBlock):
     TO-DO: document what your XBlock does.
     """
 
-    # Fields are defined on the class.  You can access them in your code as
-    # self.<fieldname>.
+    display_name = String(display_name="Display Name",
+                          default="Poll XBlock",
+                          scope=Scope.settings,
+                          help="Name of the component in the edX platform")
+
+    voted = Boolean(help="Whether this student has voted on the poll", scope=Scope.user_state, default=False)
+    poll_answer = String(help="Student answer", scope=Scope.user_state, default="")
+    poll_answers = Dict(help="All possible answers for the poll fro other students", scope=Scope.user_state_summary)
+
+    # List of answers, in the form {'id': 'some id', 'text': 'the answer text'}
+    answers = List(help="Poll answers from xml", scope=Scope.content, default=[])
+
+    question = String(help="Poll question", scope=Scope.content, default="")
 
     # TO-DO: delete count, and define your own fields.
     count = Integer(
@@ -32,12 +44,60 @@ class PollXBlock(XBlock):
         The primary view of the PollXBlock, shown to students
         when viewing courses.
         """
-        html = self.resource_string("static/html/pollxblock.html")
-        frag = Fragment(html.format(self=self))
+        template = Template(self.resource_string("static/html/pollxblock.html"))
+        html = template.render(Context({
+            'configuration_json': {
+                "reset": "true",
+                "poll_answer": "",
+                "question": "<p>Do you agree with having the right to collective self-defense under international law?</p>",
+                "answers": {"yes": "Yes", "no": "No", "other": "Can\'t say"},
+                "poll_answers": {},
+                "total": 0
+            }
+        }))
+        frag = Fragment(html)
+        #frag = Fragment(html.format(self=self))
         frag.add_css(self.resource_string("static/css/pollxblock.css"))
         frag.add_javascript(self.resource_string("static/js/src/pollxblock.js"))
         frag.initialize_js('PollXBlock')
         return frag
+
+    @XBlock.json_handler
+    def get_state(self, data, suffix=''):
+        """
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        return {"total": 0,
+                "poll_answer": "yes",
+                "poll_answers": {"yes": 1, "other": 0, "no": 0}}
+
+    @XBlock.json_handler
+    def answer_poll(self, data, suffix=''):
+        """
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        return {"total": 1,
+                "poll_answers": {"yes": 1, "other": 0, "no": 0},
+                "callback": {"objectName": "Conditional"}}
+
+    @XBlock.json_handler
+    def reset_poll(self, data, suffix=''):
+        """
+        """
+        # Just to show data coming in...
+        assert data['hello'] == 'world'
+
+        self.count += 1
+        #return {"status": "success"}
+        return {
+            'status': 'success',
+        }
 
     # TO-DO: change this handler to perform your own actions.  You may need more
     # than one handler, or you may not need any handlers at all.
