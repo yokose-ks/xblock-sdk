d217f1a3514c64fef29920e3304dffbe269b913b
yokose-ks <ayokose.gacco@gmail.com>
2014-08-12 11:57:32 +0900

    EDX-423: 4th commit

--- pollxblock/pollxblock/pollxblock.py
+++ pollxblock/pollxblock/pollxblock.py
@@ -1,6 +1,9 @@
 """TO-DO: Write a description of what this XBlock is."""
 
+import cgi
 import json
+from collections import OrderedDict
+
 import pkg_resources
 
 from xblock.core import XBlock
@@ -13,26 +16,16 @@ class PollXBlock(XBlock):
     """
     TO-DO: document what your XBlock does.
     """
-
-    display_name = String(display_name="Display Name",
-                          default="Poll XBlock",
-                          scope=Scope.settings,
-                          help="Name of the component in the edX platform")
+    display_name = String(help="Name of the component in the edX platform", scope=Scope.settings, default="Poll XBlock")
 
     voted = Boolean(help="Whether this student has voted on the poll", scope=Scope.user_state, default=False)
     poll_answer = String(help="Student answer", scope=Scope.user_state, default="")
     poll_answers = Dict(help="All possible answers for the poll fro other students", scope=Scope.user_state_summary)
 
     # List of answers, in the form {'id': 'some id', 'text': 'the answer text'}
-    answers = List(help="Poll answers from xml", scope=Scope.content, default=[])
-
-    question = String(help="Poll question", scope=Scope.content, default="")
-
-    # TO-DO: delete count, and define your own fields.
-    count = Integer(
-        default=0, scope=Scope.user_state,
-        help="A simple counter, to show something happening",
-    )
+    answers = List(help="Poll answers", scope=Scope.content, default=[{'id': 'yes', 'text': 'Yes'}, {'id': 'no', 'text': 'No'}, {'id': 'other', 'text': 'Can\'t say'}])  #default=[])
+    question = String(help="Poll question", scope=Scope.content, default="Do you agree with having the right to collective self-defense under international law?")  #default="")
+    reset = Boolean(help="Can reset/revote many time", scope=Scope.content, default=False)
 
     def resource_string(self, path):
         """Handy helper for getting resources from our kit."""
@@ -47,15 +40,7 @@ class PollXBlock(XBlock):
         """
         template = Template(self.resource_string("static/html/pollxblock.html"))
         html = template.render(Context({
-            'configuration_json': json.dumps({
-                "answers": {"yes": "Yes", "no": "No", "other": "Can\'t say"},
-                "question": "<p>Do you agree with having the right to collective self-defense under international law?</p>",
-                "poll_answer": self.poll_answer,
-                "poll_answers": self.poll_answers if self.voted else {},
-                "total": sum(self.poll_answers.values()),
-                # TODO: str(self.descriptor.xml_attributes.get('reset', 'true')).lower()
-                "reset": "true"
-            })
+            'configuration_json': self.dump_poll(),
         }))
         frag = Fragment(html)
         frag.add_css(self.resource_string("static/css/pollxblock.css"))
@@ -63,46 +48,43 @@ class PollXBlock(XBlock):
         frag.initialize_js('PollXBlock')
         return frag
 
-#    def dump_poll(self):
-#        """Dump poll information.
-#
-#        Returns:
-#            string - Serialize json.
-#        """
-#        # FIXME: hack for resolving caching `default={}` during definition
-#        # poll_answers field
-#        if self.poll_answers is None:
-#            self.poll_answers = {}
-#
-#        answers_to_json = OrderedDict()
-#
-#        # FIXME: fix this, when xblock support mutable types.
-#        # Now we use this hack.
-#        temp_poll_answers = self.poll_answers
-#
-#         # Fill self.poll_answers, prepare data for template context.
-#        for answer in self.answers:
-#            # Set default count for answer = 0.
-#            if answer['id'] not in temp_poll_answers:
-#                temp_poll_answers[answer['id']] = 0
-#            answers_to_json[answer['id']] = cgi.escape(answer['text'])
-#        self.poll_answers = temp_poll_answers
-#
-#        return json.dumps({'answers': answers_to_json,
-#            'question': cgi.escape(self.question),
-#            # to show answered poll after reload:
-#            'poll_answer': self.poll_answer,
-#            'poll_answers': self.poll_answers if self.voted else {},
-#            'total': sum(self.poll_answers.values()) if self.voted else 0,
-#            'reset': str(self.descriptor.xml_attributes.get('reset', 'true')).lower()})
+    def dump_poll(self):
+        """Dump poll information.
+
+        Returns:
+            string - Serialize json.
+        """
+        # FIXME: hack for resolving caching `default={}` during definition
+        # poll_answers field
+        if self.poll_answers is None:
+            self.poll_answers = {}
+
+        answers_to_json = OrderedDict()
+
+        # FIXME: fix this, when xblock support mutable types.
+        # Now we use this hack.
+        temp_poll_answers = self.poll_answers
+
+         # Fill self.poll_answers, prepare data for template context.
+        for answer in self.answers:
+            # Set default count for answer = 0.
+            if answer['id'] not in temp_poll_answers:
+                temp_poll_answers[answer['id']] = 0
+            answers_to_json[answer['id']] = cgi.escape(answer['text'])
+        self.poll_answers = temp_poll_answers
+
+        return json.dumps({'answers': answers_to_json,
+            'question': cgi.escape(self.question),
+            # to show answered poll after reload:
+            'poll_answer': self.poll_answer,
+            'poll_answers': self.poll_answers if self.voted else {},
+            'total': sum(self.poll_answers.values()) if self.voted else 0,
+            'reset': str(self.reset)})
 
     @XBlock.json_handler
     def get_state(self, data, suffix=''):
         """
         """
-        #return {"total": 0,
-        #        "poll_answer": "yes",
-        #        "poll_answers": {"yes": 1, "other": 0, "no": 0}}
         return {'poll_answer': self.poll_answer,
                 'poll_answers': self.poll_answers,
                 'total': sum(self.poll_answers.values())}
@@ -112,14 +94,28 @@ class PollXBlock(XBlock):
         """
         """
         poll_answer = data['poll_answer']
-        self.poll_answers[poll_answer] += 1
+
+        # FIXME: fix this, when xblock will support mutable types.
+        # Now we use this hack.
+        temp_poll_answers = self.poll_answers
+        temp_poll_answers[poll_answer] += 1
+        self.poll_answers = temp_poll_answers
 
         self.voted = True
         self.poll_answer = poll_answer
-
-        return {"total": 1,
-                "poll_answers": {"yes": 1, "other": 0, "no": 0},
-                "callback": {"objectName": "Conditional"}}
+        return {'poll_answers': self.poll_answers,
+                'total': sum(self.poll_answers.values()),
+                'callback': {'objectName': 'Conditional'}}
+#        poll_answer = data['poll_answer']
+#        #if poll_answer in self.poll_answers and not self.voted:
+#        self.poll_answers[poll_answer] += 1
+#
+#        self.voted = True
+#        self.poll_answer = poll_answer
+#
+#        return {"total": 1,
+#                "poll_answers": {"yes": 1, "other": 0, "no": 0},
+#                "callback": {"objectName": "Conditional"}}
 
     @XBlock.json_handler
     def reset_poll(self, data, suffix=''):
@@ -133,19 +129,6 @@ class PollXBlock(XBlock):
         return {
             'status': 'success',
         }
-
-    # TO-DO: change this handler to perform your own actions.  You may need more
-    # than one handler, or you may not need any handlers at all.
-    @XBlock.json_handler
-    def increment_count(self, data, suffix=''):
-        """
-        An example handler, which increments the data.
-        """
-        # Just to show data coming in...
-        assert data['hello'] == 'world'
-
-        self.count += 1
-        return {"count": self.count}
 
     # TO-DO: change this to create the scenarios you'd like to see in the
     # workbench while developing your XBlock.
